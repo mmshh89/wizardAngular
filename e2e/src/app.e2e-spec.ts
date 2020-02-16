d0e1dc29593056942dd837a5d6af978322b3795f
mmshh89 <mshni.king@gmail.com>
2020-02-16 19:43:00 +0330

    final

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('rahnamaMe-start app is running!');
+    expect(page.getTitleText()).toEqual('wizardAngular app is running!');
   });
 
   afterEach(async () => {
